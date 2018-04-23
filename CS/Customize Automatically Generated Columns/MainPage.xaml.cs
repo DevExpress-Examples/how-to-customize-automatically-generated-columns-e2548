36ba9a2cc2a92210d1c861402e9c186cb418f434
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Customize Automatically Generated Columns/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+
+namespace Customize_Automatically_Generated_Columns {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = IssueList.GetData();
+        }
+        private void grid_ColumnsPopulated(object sender, RoutedEventArgs e) {
+            foreach(GridColumn column in grid.Columns)
+                if(column.FieldName == "ID")
+                    column.Visible = false;
+            view.VisibleColumns[0].SortOrder = DevExpress.Data.ColumnSortOrder.Descending;
+        }
+    }
+    public class IssueList {
+        static public List<IssueDataObject> GetData() {
+            List<IssueDataObject> data = new List<IssueDataObject>();
+            data.Add(new IssueDataObject()
+            {
+                ID = 0,
+                IssueName = "Transaction History",
+                IssueType = "Bug",
+                IsPrivate = true
+            });
+            data.Add(new IssueDataObject()
+            {
+                ID = 1,
+                IssueName = "Ledger: Inconsistency",
+                IssueType = "Bug",
+                IsPrivate = false
+            });
+            data.Add(new IssueDataObject()
+            {
+                ID = 2,
+                IssueName = "Data Import",
+                IssueType = "Request",
+                IsPrivate = false
+            });
+            data.Add(new IssueDataObject()
+            {
+                ID = 3,
+                IssueName = "Data Archiving",
+                IssueType = "Request",
+                IsPrivate = true
+            });
+            return data;
+        }
+    }
+    public class IssueDataObject {
+        public int ID { get; set; }
+        public string IssueName { get; set; }
+        public string IssueType { get; set; }
+        public bool IsPrivate { get; set; }
+    }
+}
---
+++ VB/Customize Automatically Generated Columns/MainPage.xaml.vb
@@ -0,0 +1,71 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Grid
+
+Namespace Customize_Automatically_Generated_Columns
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = IssueList.GetData()
+		End Sub
+		Private Sub grid_ColumnsPopulated(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			For Each column As GridColumn In grid.Columns
+				If column.FieldName = "ID" Then
+					column.Visible = False
+				End If
+			Next column
+			view.VisibleColumns(0).SortOrder = DevExpress.Data.ColumnSortOrder.Descending
+		End Sub
+	End Class
+	Public Class IssueList
+		Public Shared Function GetData() As List(Of IssueDataObject)
+			Dim data As New List(Of IssueDataObject)()
+			data.Add(New IssueDataObject() With {.ID = 0, .IssueName = "Transaction History", .IssueType = "Bug", .IsPrivate = True})
+			data.Add(New IssueDataObject() With {.ID = 1, .IssueName = "Ledger: Inconsistency", .IssueType = "Bug", .IsPrivate = False})
+			data.Add(New IssueDataObject() With {.ID = 2, .IssueName = "Data Import", .IssueType = "Request", .IsPrivate = False})
+			data.Add(New IssueDataObject() With {.ID = 3, .IssueName = "Data Archiving", .IssueType = "Request", .IsPrivate = True})
+			Return data
+		End Function
+	End Class
+	Public Class IssueDataObject
+		Private privateID As Integer
+		Public Property ID() As Integer
+			Get
+				Return privateID
+			End Get
+			Set(ByVal value As Integer)
+				privateID = value
+			End Set
+		End Property
+		Private privateIssueName As String
+		Public Property IssueName() As String
+			Get
+				Return privateIssueName
+			End Get
+			Set(ByVal value As String)
+				privateIssueName = value
+			End Set
+		End Property
+		Private privateIssueType As String
+		Public Property IssueType() As String
+			Get
+				Return privateIssueType
+			End Get
+			Set(ByVal value As String)
+				privateIssueType = value
+			End Set
+		End Property
+		Private privateIsPrivate As Boolean
+		Public Property IsPrivate() As Boolean
+			Get
+				Return privateIsPrivate
+			End Get
+			Set(ByVal value As Boolean)
+				privateIsPrivate = value
+			End Set
+		End Property
+	End Class
+End Namespace
